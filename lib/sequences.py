95f06807c910b6385c0382bdc86728df2a346fbe
steve-the-hermit <stevemacharia309@gmail.com>
2023-08-27 19:45:24 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
+    fib_sequence = [0, 1] if length > 1 else [0] if length == 1 else []
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+    print(fib_sequence)
+
     pass
